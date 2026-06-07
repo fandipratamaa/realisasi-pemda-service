e4b454c0aded48952030453e4c9f96da58407357
Affandi Pratama <affandipratama45@gmail.com>
2026-06-07 17:35:26 +0700

    fix test error

--- src/test/java/cc/kertaskerja/TestRealisasiServiceApplication.java
+++ src/test/java/cc/kertaskerja/TestRealisasiServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestRealisasiServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(RealisasiService::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(RealisasiService::main).run(args);
 	}
 
 }
